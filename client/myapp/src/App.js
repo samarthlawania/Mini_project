97eb656465d4ea585b0855bb174ced93462c5583
vedant singh <93176679+Lonewolf123457499@users.noreply.github.com>
2023-11-14 10:22:59 +0000

    commited

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,13 +1,63 @@
-import './App.css';
+import React from 'react';
+import { Outlet, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Home, Login, Profile, Register, ResetPassword } from './pages';
+import { useSelector } from 'react-redux';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error('Error caught by error boundary:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong. Please try again later.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+function Layout() {
+  const {user} = useSelector((state)=>state.user); // Assuming you'll set this dynamically
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
 
 function App() {
+  const {theme}=useSelector((state)=>state.theme);
+  console.log(theme);
   return (
-    <div className="App">
-      <h1 className="text-xl font-bold underline ">
-        Hello world!
-      </h1>
+    
+      <div data-theme={theme} className='w-full min-h-[100vh]'>
+        {/* <h1 className="bg-red-500">App pages </h1> */}
+        <ErrorBoundary>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile/:id?" element={<Profile />} />
+            </Route>
+
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+          </Routes>
+        </ErrorBoundary>
+      </div>
    
-    </div>
   );
 }
 
---
+++ client/myapp/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='bg-bgColor w-full h-[100vh] flex items-centre justify-centre p-6'>
+      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
+        <div className='w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center '></div>
+        <div className='w-full flex gap-2 items-center mb-6'>
+          <div className='p-2 bg-[#065ad8] rounded text-white'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
